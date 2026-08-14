5163cd2435cdf391ffecfd24ed606b078afd0e82
Zheng (Joe) Dong <32425080+functionalepigenomics@users.noreply.github.com>
2026-08-14 15:11:29 -0500

    Create liftOver.sh

---
+++ Analysis/Genomic feature enrichment/liftOver.sh
@@ -0,0 +1,5 @@
+liftOver \
+  wgEncodeBroadHmmGm12878HMM.bed \
+  /scratch/zdong/Projects/PanEpiG/V1-9/LDSC/hg19ToHg38.over.chain.gz \
+  wgEncodeBroadHmmGm12878HMM.hg38.bed \
+  wgEncodeBroadHmmGm12878HMM.unmapped.bed
